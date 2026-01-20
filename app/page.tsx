e17e55ddc24200fbb68697d3b35fe88cbad2b6c5
VerrillAng <verrill.ang@gmail.com>
2026-01-19 17:49:47 -0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,69 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import { ReactLenis, useLenis } from "lenis/react";
+import { useWindowSize } from "../hooks/useWindowSize";
+import Laptop from "../components/laptop";
+
+import Spline, { type SPEObject } from "@splinetool/react-spline";
+import { type Application } from "@splinetool/runtime";
+import { useScroll, useTransform, motion } from "framer-motion";
+
 import Image from "next/image";
 
-export default function Home() {
+import Picture1 from "../public/images/akaza.jpg";
+
+function Page() {
+  const lenis = useLenis((lenis) => {
+    // called every scroll
+    // console.log(lenis);
+  });
+
+  // Get Window Size
+  const { width, height } = useWindowSize();
+
+  // // Framer Motion
+  // const container = useRef<HTMLDivElement | null>(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: container,
+  //   offset: ["start start", "end end"],
+  // });
+
+  // const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <ReactLenis root />
+      {/* <div ref={container} className="relative h-[300vh]">
+        <div className="sticky top-0 h-[100vh] bg-orange-500">
+          <div className="el">
+            <motion.div style={{ scale: scale4 }} className="imageContainer">
+              <Image src={Picture1} fill alt="image" placeholder="blur" />
+            </motion.div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div> */}
+      {/* <div ref={container} className="relative h-[calc(100vh+700px)]">
+        <div className="sticky top-0 h-[100vh] bg-orange-500">
+          <Laptop width={width} height={height}></Laptop>
         </div>
-      </main>
-    </div>
+      </div> */}
+
+      <div>
+        <div className="h-[100vh] w-[100vw] bg-gradient-to-br from-stone-950 via-gray-900 to-stone-950">
+          <header className="min-h-screen flex flex-col items-center justify-center ">
+            <h1 className="text-5xl md:text-7xl font-bold mb-4 bg-gradient-to-r from-blue-400 to-cyan-400 bg-clip-text text-transparent">
+              Hi, I'm Verrill
+            </h1>
+            <h2 className="text-lg md:text-xl text-gray-300">
+              Passionate about Software Development and Artificial Intelligence.
+            </h2>
+          </header>
+        </div>
+      </div>
+    </>
   );
 }
+
+export default Page;
