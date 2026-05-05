173eeb0502b84aed14359bbb26aa032827956956
Verrill <verrill.ang@gmail.com>
2026-05-05 13:28:59 -0700

    Finish bezel with inner shadows, and changed desktop color

--- components/home_about_me.tsx
+++ components/home_about_me.tsx
@@ -3,8 +3,10 @@ import styles from "./home_about_me.module.css";
 function HomeAboutMe() {
   return (
     <div className="h-[100vh] w-[100vw] bg-[#F9EFDD] p-6">
-      <div className={`w-full h-full rounded-2xl ${styles.window}`}>
-        {/* content goes here */}
+      <div className={`h-[95%] w-full rounded-2xl ${styles.window}`}>
+        <div className="h-full w-full rounded-2xl bg-[#FBF0D9]">
+
+        </div>
       </div>
     </div>
   );
